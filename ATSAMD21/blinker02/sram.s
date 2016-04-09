35aa25008457cb5981634a95ecb6c6095e29e913
dwelch <dwelch@dwelch.com>
2016-04-09 11:09:49 -0400

    this chip has some registers that are only 8 and 16 bit

--- ATSAMD21/blinker02/sram.s
+++ ATSAMD21/blinker02/sram.s
@@ -1,8 +1,7 @@
 
-;@-----------------------
 .cpu cortex-m0
 .thumb
-;@-----------------------
+
 .thumb_func
 .global _start
 _start:
@@ -12,32 +11,49 @@ _start:
     b hang
 .thumb_func
 hang:   b .
-;@-----------------------
+
 .align
 stacktop: .word 0x20001000
-;@-----------------------
+
+.thumb_func
+.globl PUT8
+PUT8:
+    strb r1,[r0]
+    bx lr
+
 .thumb_func
 .globl PUT16
 PUT16:
     strh r1,[r0]
     bx lr
-;@-----------------------
+
 .thumb_func
 .globl PUT32
 PUT32:
     str r1,[r0]
     bx lr
-;@-----------------------
+
+.thumb_func
+.globl GET8
+GET8:
+    ldrb r0,[r0]
+    bx lr
+
+.thumb_func
+.globl GET16
+GET16:
+    ldrh r0,[r0]
+    bx lr
+
 .thumb_func
 .globl GET32
 GET32:
     ldr r0,[r0]
     bx lr
-;@-----------------------
+
 .thumb_func
 .globl dummy
 dummy:
     bx lr
-;@-----------------------
+
 .end
-;@-----------------------
